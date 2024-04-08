d44bea862e200eaef9e44dfce58ca888376d166a
Nyaarium <project@nyaarium.com>
2024-04-08 15:06:36 -0700

    StrictMode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import { QueryProvider } from "@/schemas/default/useGraphAPI";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { StrictMode } from "react";
 
 const darkTheme = createTheme({
 	palette: {
@@ -13,14 +13,16 @@ const darkTheme = createTheme({
 
 export default function App({ Component, pageProps }) {
 	return (
-		<SnackbarProvider>
-			<QueryProvider>
-				<ThemeProvider theme={darkTheme}>
-					<CssBaseline />
-					<Component {...pageProps} />
-				</ThemeProvider>
-			</QueryProvider>
-		</SnackbarProvider>
+		<StrictMode>
+			<SnackbarProvider>
+				<QueryProvider>
+					<ThemeProvider theme={darkTheme}>
+						<CssBaseline />
+						<Component {...pageProps} />
+					</ThemeProvider>
+				</QueryProvider>
+			</SnackbarProvider>
+		</StrictMode>
 	);
 }
 
